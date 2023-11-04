94cb746cce6a4740a0d8b41be34856b330af1491
uadam <149405030+umar-adam@users.noreply.github.com>
2023-11-04 12:39:16 +0000

    Delete states_image.py

--- states_image.py
+++
@@ -1,3 +0,0 @@
-# Here you will have to download and save the follwing image:
-https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/National-atlas-blank-state-outlines.png/480px-National-atlas-blank-state-outlines.png
-# Save the file title as ' blank_states_img.gif ' in the same location as the main.py and 50_states.csv files
